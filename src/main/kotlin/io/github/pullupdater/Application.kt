9d2f5436f3f4186f86421b8d6b071a26f6601148
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-23 16:01:25 +0200

    Use port 4545

--- src/main/kotlin/io/github/pullupdater/Application.kt
+++ src/main/kotlin/io/github/pullupdater/Application.kt
@@ -7,7 +7,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 443, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 4545, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
