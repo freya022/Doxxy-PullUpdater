cb7128b857856d2d806b3f333914d8eae05b5ec3
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-23 11:18:04 +0200

    Use type safe routing

--- src/main/kotlin/io/github/pullupdater/plugins/Routing.kt
+++ src/main/kotlin/io/github/pullupdater/plugins/Routing.kt
@@ -3,9 +3,12 @@ package io.github.pullupdater.plugins
 import io.github.pullupdater.routes.updaterRouting
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.resources.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(Resources)
+
     routing {
         authenticate("auth-bearer") {
             updaterRouting()
--- src/main/kotlin/io/github/pullupdater/routes/UpdaterRoutes.kt
+++ src/main/kotlin/io/github/pullupdater/routes/UpdaterRoutes.kt
@@ -2,29 +2,24 @@ package io.github.pullupdater.routes
 
 import io.github.pullupdater.JDAFork
 import io.ktor.http.*
+import io.ktor.resources.*
 import io.ktor.server.application.*
+import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+@Resource("/update")
+class PullRequestUpdate() {
+    @Suppress("unused")
+    @Resource("/{pullNumber}")
+    class Number(val parent: PullRequestUpdate = PullRequestUpdate(), val pullNumber: Int)
+}
+
 fun Route.updaterRouting() {
-    route("/update") {
-        get("{pull_number}") {
-            val prNumber = let {
-                val prNumberStr = call.parameters["pull_number"] ?: return@get call.respondText(
-                    "Missing PR number",
-                    status = HttpStatusCode.BadRequest
-                )
-
-                prNumberStr.toIntOrNull() ?: return@get call.respondText(
-                    "Must be a number",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-
-            val result = runCatching {
-                JDAFork.requestUpdate(prNumber)
-            }.getOrElse { JDAFork.Result.fail(HttpStatusCode.InternalServerError, "Unknown error while updating PR") }
-            call.respondText(contentType = ContentType.Application.Json, status = result.statusCode, text = result.body)
-        }
+    get<PullRequestUpdate.Number> { pullRequest ->
+        val result = runCatching {
+            JDAFork.requestUpdate(pullRequest.pullNumber)
+        }.getOrElse { JDAFork.Result.fail(HttpStatusCode.InternalServerError, "Unknown error while updating PR") }
+        call.respondText(contentType = ContentType.Application.Json, status = result.statusCode, text = result.body)
     }
 }
